a335ad86038e63d65c4612060d1f3078f9af30b4
ceyed <saeed.sarmad.976@gmail.com>
2023-04-03 16:12:25 +0330

    fix: loop range was 3 for testing

--- Controller/translate_chapter_name.py
+++ Controller/translate_chapter_name.py
@@ -6,6 +6,6 @@ from tqdm import tqdm
 def translate_chapter_names(links):
     translator = Translator()
     new_links = []
-    for index in tqdm(range(0, round(len(links) / 80))):
+    for index in tqdm(range(0, len(links))):
         new_links.append((translator.translate(links[index].text).text, links[index].get('href')))
     return new_links
